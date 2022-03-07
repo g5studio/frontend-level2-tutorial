d1df28e7031215025cab5705d6f8b29dac733b40
William Chiang <william.chiang@tcghl.com>
2022-03-07 18:06:07 +0800

    新增判斷式範例與迴圈練習題

--- week3/main.js
+++ week3/main.js
@@ -5,19 +5,103 @@ function resolveAction(id) {
         document.getElementById('article').style.display = 'none';
     }
     switch (id) {
-        case 'example-function':
+        case 'example-if':
             showHint(`
-            <p>以下範例將示範如何宣告函式並回傳值。</P>
+            <p>以下範例將示範如何製作基本判斷式。</P>
             <p class = "text-warning mt-1">
-                function getUserName(name = 'William') {<br>
-                    &nbsp&nbsp var userName = name;<br>
-                    &nbsp&nbsp userName = 'User:' + name;<br>
-                    &nbsp&nbsp return userName;<br>
-                }<br>
+                function conditionalStatementExample(color = 'black') {<br>
+                    &nbsp&nbsp  if (color === 'black') {<br>
+                    &nbsp&nbsp&nbsp&nbsp    console.log('黑');<br>
+                    &nbsp&nbsp  } else {<br>
+                    &nbsp&nbsp&nbsp&nbsp    console.log('白')<br>
+                    &nbsp&nbsp}<br>
+                }
             </p>
-            <p class = "text-xs text-hint mt-1">點擊開發者工具中的console標籤，呼叫getUserName()函數。</P>
+            <p class = "text-xs text-hint mt-1">
+                點擊開發者工具中的console標籤，呼叫conditionalStatementExample()函數，傳入參數查看結果。
+            </P>
             `);
             break;
+        case 'example-else-if':
+            showHint(`
+            <p>以下範例將示範如何搭配else-if實做判斷式。</P>
+            <p class = "text-warning mt-1">
+                function multipleConditionalStatementExample(color = 'red') {<br>
+                    &nbsp&nbsp  if (color === 'red') {<br>
+                    &nbsp&nbsp&nbsp&nbsp    console.log('紅色');<br>
+                    &nbsp&nbsp  } else if (color === 'blue') {<br>
+                    &nbsp&nbsp&nbsp&nbsp   console.log('藍色')<br>
+                    &nbsp&nbsp  } else {<br>
+                    &nbsp&nbsp&nbsp&nbsp    console.log('黑色')<br>
+                    &nbsp&nbsp}<br>
+                }
+            </p>
+            <p class = "text-xs text-hint mt-1">
+                點擊開發者工具中的console標籤，呼叫conditionalStatementExample()函數，傳入參數查看結果。
+            </P>
+            `);
+            break;
+        case 'practice-for':
+            findOdds(document.getElementById('practice-for-input').value);
+            break;
+    }
+}
+
+/**
+ * @description 判斷式範例
+ */
+function conditionalStatementExample(color = 'black') {
+    if (color === 'black') {
+        console.log('黑');
+    } else {
+        console.log('白');
+    }
+}
+
+/**
+ * @description 多條件判斷式範例
+ */
+function multipleConditionalStatementExample(color = 'red') {
+    if (color === 'red') {
+        console.log('紅色');
+    } else if (color === 'blue') {
+        console.log('藍色')
+    } else {
+        console.log('黑色')
+    }
+}
+
+/**
+ * @description 迴圈範例 - while
+ */
+function whileExample() {
+    while (true) {
+        // infinite loop
+    }
+}
+
+/**
+ * @description 迴圈範例 - for
+ */
+function forExample() {
+    for (let i of [1, 2, 3, 4]) {
+        //...do something
+    }
+}
+
+/**
+ * @description 練習題 - 找出奇數
+ */
+function findOdds(maximum) {
+    var isHaveResult = false;
+    for (let current = 0; current <= maximum; current = current + 1) {
+        if (current % 2 !== 0) {
+            console.log(current);
+            isHaveResult = true;
+        }
+    }
+    if (!isHaveResult) {
+        console.log('查無結果');
     }
 }
 
